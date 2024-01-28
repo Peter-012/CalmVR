abbf9c96abb11a5b08d6d6149908276c6b9cb566
Peter-012 <151083213+Peter-012@users.noreply.github.com>
2024-01-28 00:58:13 -0500

    UI reacts to laser but no ray cast effect on laser.

--- Assets/Scripts/VRInputModule.cs
+++ Assets/Scripts/VRInputModule.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
 
-public class VRInputModule : MonoBehaviour
+public class VRInputModule : BaseInputModule
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera Cam;
+    public SteamVR_Input_Sources TargetSource;
+    public SteamVR_Action_Boolean ClickAction;
+
+    private GameObject CurrentObject = null;
+    private PointerEventData Data = null;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+
+        Data = new PointerEventData(eventSystem);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Process()
     {
-        
+        Data.Reset();
+        Data.position = new Vector2(Cam.pixelWidth / 2, Cam.pixelHeight / 2);
+
+        eventSystem.RaycastAll(Data, m_RaycastResultCache);
+        Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        CurrentObject = Data.pointerCurrentRaycast.gameObject;
+
+        m_RaycastResultCache.Clear();
+
+        HandlePointerExitAndEnter(Data, CurrentObject);
+        if (ClickAction.GetStateDown(TargetSource))
+        {
+            ProcessPress(Data);
+        }
+        if (ClickAction.GetStateDown(TargetSource))
+        {
+            ProcessPress(Data);
+        }
+    }
+
+    public PointerEventData GetData()
+    {
+        return Data;
+    }
+
+    private void ProcessPress(PointerEventData data)
+    { 
+    
+    }
+    private void ProcessRelease(PointerEventData data)
+    {
+
     }
 }
